b5264d10336d30952081ee8a288eb32624b81987
Jo <jo@precintl.com>
2017-09-06 15:03:07 +0800

    module

---
+++ application/build.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jo
+ * Date: 6/9/2017
+ * Time: 14:56
+ */
+
+return [
+    'api' => [
+        '__dir__'    => ['controller', 'model', 'view'],
+        'controller' => ['index'],
+        'model'      => ['game', 'gym', 'user'],
+//        'view'       => ['index/index', 'index/test'],
+    ],
+];
