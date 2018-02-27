92fdf2a9add900242549e24c96b99adf160c504a
Jo <jo@precintl.com>
2018-02-27 16:07:06 +0800

    Worker Class

---
+++ application/push/controller/Worker.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jo
+ * Date: 27/2/2018
+ * Time: 15:48
+ */
+
+//namespace app\api\controller;
+namespace app\push\controller;
+
+use think\worker\Server;
+
+class Worker extends Server
+{
+//    protected $socket = 'tcp://0.0.0.0:2347';
+    protected $socket = 'websocket://127.0.0.1:2346';
+
+    /**
+     * 收到信息
+     * @param $connection
+     * @param $data
+     */
+    public function onMessage($connection, $data)
+    {
+        $connection->send('我收到你的信息了');
+    }
+
+    /**
+     * 当连接建立时触发的回调函数
+     * @param $connection
+     */
+    public function onConnect($connection)
+    {
+        echo 'jjjj';
+    }
+
+    /**
+     * 当连接断开时触发的回调函数
+     * @param $connection
+     */
+    public function onClose($connection)
+    {
+
+    }
+
+    /**
+     * 当客户端的连接上发生错误时触发
+     * @param $connection
+     * @param $code
+     * @param $msg
+     */
+    public function onError($connection, $code, $msg)
+    {
+        echo "error $code $msg\n";
+    }
+
+    /**
+     * 每个进程启动
+     * @param $worker
+     */
+    public function onWorkerStart($worker)
+    {
+
+    }
+}
---
+++ server.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jo
+ * Date: 27/2/2018
+ * Time: 15:41
+ */
+
+#!/usr/bin/env php
+
+define('APP_PATH', __DIR__ . '/application/');
+define('BIND_MODULE','push/Worker');
+// 加载框架引导文件
+require __DIR__ . '/thinkphp/start.php';
